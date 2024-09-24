aa8d02875cb0ff3e468227ed592ece87d48faf05
Erick de la Cruz <erick_1322134389@uptecamac.edu.mx>
2024-09-24 12:48:48 -0600

    Agregando funcionalidad para obtener más pokemones desde el servicio

--- src/app/pages/list-pokemons/list-pokemons.page.ts
+++ src/app/pages/list-pokemons/list-pokemons.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Pokemon } from 'src/app/models/pokemon';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-list-pokemons',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListPokemonsPage implements OnInit {
 
-  constructor() { }
+  public pokemons: Pokemon[];
+
+  constructor(private pokemonService:PokemonService) { 
+    this.pokemons = [];
+  }
 
   ngOnInit() {
+    this.morePokemon();
+  }
+
+  morePokemon() {
+
+    const promise = this.pokemonService.getPokemons();
+
+    if (promise) {
+      promise.then((result: Pokemon[]) => {
+        console.log(result);
+
+        this.pokemons = this.pokemons.concat(result);
+        console.log(this.pokemons);
+        
+        
+      });
+    }
+    
   }
 
 }
