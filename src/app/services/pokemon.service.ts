46aa4f540e0f2fdf66a739b6c2134fde7c86daa1
Erick de la Cruz <erick_1322134389@uptecamac.edu.mx>
2024-09-24 11:23:14 -0600

    Obtiendo pokemones con CapacitorHttp

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,4 +1,7 @@
+import { HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { CapacitorHttp } from '@capacitor/core';
+import { Pokemon } from '../models/pokemon';
 
 @Injectable({
   providedIn: 'root',
@@ -11,11 +14,47 @@ export class PokemonService {
   }
 
   getPokemons() {
+
     const url = this.nextUrl;
 
     if (url) {
-    }
+      const options = {
+        url,
+        headers: {},
+        params: {},
+      };
 
+      return CapacitorHttp.get(options).then(async (response) => {
+        let pokemons: Pokemon[] = [];
+
+        console.log(response);
+
+        if (response.data) {
+          const results = response.data.results;
+          this.nextUrl = response.data.next;
+
+          const promises: Promise<HttpResponse>[] = [];
+
+          for (let index = 0; index < results.length; index++) {
+            const pokemon = results[index];
+            const urlPokemon = pokemon.url;
+            const options = {
+              url: urlPokemon,
+              headers: {},
+              params: {},
+            };
+            promises.push(CapacitorHttp.get(options));
+          }
+
+          await Promise.all(promises).then((responses) => {
+            console.log(responses);
+          });
+        }
+
+        return pokemons;
+      });
+
+    }
     return null;
   }
 }
