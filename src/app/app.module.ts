3388b3eeb69b50fe51d7e56ee468f47642b0d27a
Erick de la Cruz <erick_1322134389@uptecamac.edu.mx>
2024-09-24 20:56:38 -0600

    Bloqueando la orientación de la pantalla

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,14 @@ import { IonicModule, IonicRouteStrategy, NavParams } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ScreenOrientation } from '@awesome-cordova-plugins/screen-orientation/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, NavParams],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, NavParams,
+    ScreenOrientation
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
